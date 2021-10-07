562aa2c63a135c7ed03d4743efbe9cfd8558ace4
Paberu <paberu.tocador@gmail.com>
2021-10-07 21:21:07 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -55,6 +55,8 @@ class LinkedList:
                     break
             previous = node
             node = node.next
+        if self.head is None:
+            self.tail = None
 
     def clean(self):
         self.head = None
