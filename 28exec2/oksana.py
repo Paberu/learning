d70ff6fb13d53a723705a12a7670644bb72426b6
Paberu <paberu.tocador@gmail.com>
2021-10-10 19:04:51 +0300

    Update oksana.py

--- 28exec2/oksana.py
+++ 28exec2/oksana.py
@@ -1,5 +1,7 @@
 def odometer(oksana):
     s = 0
+    t = 0
     for i in range(0, len(oksana), 2):
-        s += oksana[i] * oksana[i+1]
+        s += oksana[i] * (oksana[i+1] - t)
+        t = oksana[i+1]
     return s
