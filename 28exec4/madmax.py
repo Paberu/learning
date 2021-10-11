296cb30d7d261a830dd44be4c42ebdbc45d6b1a1
Paberu <paberu.tocador@gmail.com>
2021-10-11 22:06:02 +0300

    Update madmax.py

--- 28exec4/madmax.py
+++ 28exec4/madmax.py
@@ -1,14 +1,14 @@
-def madMax(n, tele):
-    min_value, max_value = 0, max(tele)
-    inter = [0]*n
-    inter[int((n-1)/2)] = max_value
-    tele.remove(max_value)
-    for i in range(int((n - 1) / 2)):
-        min_value = min(tele)
+def MadMax(N, Tele):
+    min_value, max_value = 0, max(Tele)
+    inter = [0]*N
+    inter[int((N-1)/2)] = max_value
+    Tele.remove(max_value)
+    for i in range(int((N - 1) / 2)):
+        min_value = min(Tele)
         inter[i] = min_value
-        tele.remove(min_value)
-    for i in range(int((n - 1) / 2)):
-        max_value = max(tele)
-        inter[int((n-1)/2)+i+1] = max_value
-        tele.remove(max_value)
+        Tele.remove(min_value)
+    for i in range(int((N - 1) / 2)):
+        max_value = max(Tele)
+        inter[int((N-1)/2)+i+1] = max_value
+        Tele.remove(max_value)
     return inter
