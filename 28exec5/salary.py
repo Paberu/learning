10a173b22b796ca3c0a6f81d2b55d1ddcdb6d2fa
Paberu <paberu.tocador@gmail.com>
2021-10-12 10:07:40 +0300

    Update salary.py

--- 28exec5/salary.py
+++ 28exec5/salary.py
@@ -1,6 +1,7 @@
 def SynchronizingTables(N, ids, salary):
-
     salary_result = [0]*N
     for i in range(N):
-        salary_result[sorted(ids).index(ids[i])] = sorted(salary)[i]
+        salary_result[i] = sorted(salary)[sorted(ids).index(ids[i])]
     return salary_result
+
+print(SynchronizingTables(4,[10,67,68,28],[55,73,10,6]))
