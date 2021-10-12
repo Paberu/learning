e10e3f214b92654bd014a5df64921250777ab4b5
Paberu <paberu.tocador@gmail.com>
2021-10-12 11:41:35 +0300

    Update pattern.py

--- 28exec6/pattern.py
+++ 28exec6/pattern.py
@@ -1,13 +1,13 @@
 def PatternUnlock(N, hits):
     result = []
     path = 0
-    diag_nums = ((2,4),(4,2),(2,6),(6,2),(2,9),(9,2),(2,7),(7,2))
+    diag_nums = ((2, 4), (4, 2), (2, 6), (6, 2), (2, 9), (9, 2), (2, 7), (7, 2))
     for i in range(N-1):
         if (hits[i], hits[i+1]) not in diag_nums:
             path += 1
         else:
             path += 2**0.5
-    for symbol in str(round(path,5)):
+    for symbol in str(round(path, 5)):
         if symbol not in ('0', '.'):
             result.append(symbol)
     return ''.join(result)
