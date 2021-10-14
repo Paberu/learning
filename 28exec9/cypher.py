81ae6c2a2f3365299dc8c5f577d526366582fa1f
Paberu <paberu.tocador@gmail.com>
2021-10-14 17:44:37 +0300

    Update cypher.py

--- 28exec9/cypher.py
+++ 28exec9/cypher.py
@@ -1,15 +1,28 @@
 def TheRabbitsFoot(s, encode):
-    s = s.replace(' ','')
-    size = len(s)**0.5
-    rows = int(size)
-    columns = rows + 1
-    if rows * columns < len(s):
-        rows += 1
-    print(rows,' x ',columns)
-    new_s = ''
-    for i in range(columns):
-        for j in range(rows):
-            new_s = 
+    if encode:
+        s = s.replace(' ','')
+        size = len(s)**0.5
+        rows = int(size)
+        columns = rows + 1
+        if rows * columns < len(s):
+            rows += 1
+        print(rows,' x ',columns)
+        s_matrix = [''] * rows
+        for i in range(rows):
+            s_matrix[i] = ['']*columns
+        print(s_matrix)
+        for i in range(rows):
+            for j in range(columns):
+                if i*columns + j < len(s):
+                    s_matrix[i][j] = s[i*columns+j]
+                else:
+                    break
+        print(s_matrix)
+        reverted_s = [[s_matrix[j][i] for j in range(rows)] for i in range(columns)]
+        print(reverted_s)
+        s = ' '.join(''.join(x) for x in reverted_s)
+    else:
+        pass
     return s
 
 print(TheRabbitsFoot('отдай мою кроличью лапку', True))
