46c097076d1aeb30568f80a7be9270c8483502cd
Paberu <paberu.tocador@gmail.com>
2021-10-15 15:57:18 +0300

    Update cypher.py

--- 28exec9/cypher.py
+++ 28exec9/cypher.py
@@ -6,29 +6,48 @@ def TheRabbitsFoot(s, encode):
         columns = rows + 1
         if rows * columns < len(s):
             rows += 1
+            
         s_matrix = [''] * rows
         for i in range(rows):
             s_matrix[i] = ['']*columns
+            
         for i in range(rows):
             for j in range(columns):
                 if i*columns + j < len(s):
                     s_matrix[i][j] = s[i*columns+j]
                 else:
                     break
+                
         reverted_s = [[s_matrix[j][i] for j in range(rows)] for i in range(columns)]
     else:
-        parts = s.split()
-        columns = len(parts[0])
-        rows = len(parts)
+        s = s.replace(' ', '')
+        size = len(s)**0.5
+        columns = int(size)
+        rows = columns + 1
         if rows * columns < len(s):
             columns += 1
+            
         s_matrix = [''] * rows
         for i in range(rows):
             s_matrix[i] = ['']*columns
-        for i in range(columns):
-            for j in range(rows):
-                s_matrix[j][i] = parts[j][i] if i < len(parts[j]) else ''
+
+        for i in range(rows):
+            for j in range(columns):
+                if i * columns + j < len(s):
+                    s_matrix[i][j] = s[i*columns+j]
+                else:
+                    s_matrix[i][j] = ''
+
         reverted_s = [[s_matrix[j][i] for j in range(rows)] for i in range(columns)]
     s = ''.join(''.join(x) for x in reverted_s)
 
     return s
+
+
+print(TheRabbitsFoot('ahesosl', False))
+print(TheRabbitsFoot('asshole', True))
+print(TheRabbitsFoot('отдай мою кроличью лап', True))
+print(TheRabbitsFoot('омоютоллдюиаакчпйрь', False))
+
+print(TheRabbitsFoot('омоюу толл дюиа акчп йрьк', False))
+print(TheRabbitsFoot('отдай мою кроличью лапку', True))
