21abc731ab3ce5016926bed144c92ff8d40855e7
Paberu <paberu.tocador@gmail.com>
2021-10-15 20:49:08 +0300

    Update cypher.py

--- 28exec9/cypher.py
+++ 28exec9/cypher.py
@@ -1,72 +1,42 @@
 def TheRabbitsFoot(s, encode):
     if encode:
-        s = s.replace(' ','')
+        s = s.replace(' ', '')
         size = len(s)**0.5
         rows = int(size)
         columns = rows + 1
         if rows * columns < len(s):
             rows += 1
-<<<<<<< HEAD
-            
         s_matrix = [''] * rows
         for i in range(rows):
             s_matrix[i] = ['']*columns
-            
-=======
-        print(rows,' x ',columns)
-        s_matrix = [''] * rows
-        for i in range(rows):
-            s_matrix[i] = ['']*columns
-        print(s_matrix)
->>>>>>> parent of ff5eca9 (Update cypher.py)
         for i in range(rows):
             for j in range(columns):
                 if i*columns + j < len(s):
                     s_matrix[i][j] = s[i*columns+j]
                 else:
                     break
-        print(s_matrix)
         reverted_s = [[s_matrix[j][i] for j in range(rows)] for i in range(columns)]
-<<<<<<< HEAD
         s = ' '.join(''.join(x) for x in reverted_s)
     else:
-        #s = s.replace(' ', '')
-        size = len(s)**0.5
-        columns = int(size)
-        rows = columns + 1
-        if rows * columns < len(s):
+        parts = s.split()
+        columns = len(parts[0])
+        rows = len(parts)
+        if rows * columns < len(s.replace(' ', '')):
             columns += 1
-            
         s_matrix = [''] * rows
         for i in range(rows):
             s_matrix[i] = ['']*columns
-
-        for i in range(rows):
-            for j in range(columns):
-                if i * columns + j < len(s):
-                    s_matrix[i][j] = s[i*columns+j]
-                else:
-                    s_matrix[i][j] = ''
-
+        for i in range(columns):
+            for j in range(rows):
+                s_matrix[j][i] = parts[j][i] if i < len(parts[j]) else ''
         reverted_s = [[s_matrix[j][i] for j in range(rows)] for i in range(columns)]
         s = ''.join(''.join(x) for x in reverted_s)
-
     return s
 
+print(TheRabbitsFoot('однажды в студеную зимнюю порру', True))
+print(TheRabbitsFoot('оыемр двннр нсуюу атюю жузп ддио', False))
+print(TheRabbitsFoot('а б абв', True))
+print(TheRabbitsFoot('аб бв а', False))
 
-print(TheRabbitsFoot('ahe so sl', False))
-##print(TheRabbitsFoot('asshole', True))
-##print(TheRabbitsFoot('отдай мою кроличью лап', True))
-##print(TheRabbitsFoot('омоютоллдюиаакчпйрь', False))
-##
-##print(TheRabbitsFoot('омоюу толл дюиа акчп йрьк', False))
-##print(TheRabbitsFoot('отдай мою кроличью лапку', True))
-=======
-        print(reverted_s)
-        s = ' '.join(''.join(x) for x in reverted_s)
-    else:
-        pass
-    return s
-
+print(TheRabbitsFoot('омоюу толл дюиа акчп йрьк', False))
 print(TheRabbitsFoot('отдай мою кроличью лапку', True))
->>>>>>> parent of ff5eca9 (Update cypher.py)
