2a7c92064247a0a3dc2939135cde990c555a5fc4
Paberu <paberu.tocador@gmail.com>
2021-10-20 21:45:14 +0300

    Update tankrush.py

--- 28exec15/tankrush.py
+++ 28exec15/tankrush.py
@@ -14,7 +14,7 @@ def TankRush(h1, w1, s1, h2, w2, s2):
 
     for i in range(0, h1-h2+1):
         for j in range(0, w1-w2+1):
-            map_temp = [[map1[col][row] for row in range(i, i+h2)] for col in range(j, j+w2)]
+            map_temp = [[map1[row][col] for col in range(j, j+w2)] for row in range(i, i+h2)]
             if map_temp == map2:
                 return True
 
