8e6794ea780f8d1f3c7edd0ed4fa08b790485f02
Paberu <paberu.tocador@gmail.com>
2021-10-20 21:36:57 +0300

    Update tankrush.py

--- 28exec15/tankrush.py
+++ 28exec15/tankrush.py
@@ -1,7 +1,10 @@
 def TankRush(h1, w1, s1, h2, w2, s2):
+<<<<<<< HEAD
     if s1 == '' or s2 == '' or h1 == 0 or h2 == 0 or w1 == 0 or w2 == 0:
         return False
 
+=======
+>>>>>>> parent of 6a92cd1 (Update tankrush.py)
     map1 = []
     s1 = s1.split()
     for i in range(h1):
@@ -12,10 +15,19 @@ def TankRush(h1, w1, s1, h2, w2, s2):
     for i in range(h2):
         map2.append(list(s2[i]))
 
-    for i in range(0, h1-h2+1):
-        for j in range(0, w1-w2+1):
-            map_temp = [[map1[col][row] for row in range(i, i+h2)] for col in range(j, j+w2)]
-            if map_temp == map2:
-                return True
+    map_temp = [0]*h2
+    for i in range(h2):
+        map_temp[i] = [0]*w2
 
+<<<<<<< HEAD
     return False
+=======
+    for i in range(0, h1-h2):
+        for j in range(0, w1-w2):
+            map_temp[0] = map[i]
+           # print(map1[i:i+w2][j:j+w2])
+            pass
+
+
+print(TankRush(3, 4, '1234 2345 0987', 2, 2, '34 98'))
+>>>>>>> parent of 6a92cd1 (Update tankrush.py)
