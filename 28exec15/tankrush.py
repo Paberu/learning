6a92cd1eba334f1c1781b976200406200cdeb7a9
Paberu <paberu.tocador@gmail.com>
2021-10-20 11:07:57 +0300

    Update tankrush.py

--- 28exec15/tankrush.py
+++ 28exec15/tankrush.py
@@ -1,4 +1,7 @@
 def TankRush(h1, w1, s1, h2, w2, s2):
+    if s1 == '' or s2 == '' or h1 == 0 or h2 == 0 or w1 == 0 or w2 == 0:
+        return False
+    
     map1 = []
     s1 = s1.split()
     for i in range(h1):
@@ -9,15 +12,15 @@ def TankRush(h1, w1, s1, h2, w2, s2):
     for i in range(h2):
         map2.append(list(s2[i]))
 
-    map_temp = [0]*h2
-    for i in range(h2):
-        map_temp[i] = [0]*w2
+    for i in range(0, h1-h2+1):
+        for j in range(0, w1-w2+1):
+            map_temp = [[map1[col][row] for row in range(i, i+h2)] for col in range(j, j+w2)]
+            if map_temp == map2:
+                return True
 
-    for i in range(0, h1-h2):
-        for j in range(0, w1-w2):
-            map_temp[0] = map[i]
-           # print(map1[i:i+w2][j:j+w2])
-            pass
+    return False
 
 
-print(TankRush(3, 4, '1234 2345 0987', 2, 2, '34 98'))
+print(TankRush(4, 4, '1234 2345 0987 3562', 2, 2, '34 98'))
+print(TankRush(4, 4, '1234 2345 0987 3562', 4, 4, '1234 2345 0987 3562'))
+print(TankRush(2, 2, '34 98', 2, 2, '34 98'))
