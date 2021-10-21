07ae56fd9af77cedfc7d556d88e894e61fcd01c7
Paberu <paberu.tocador@gmail.com>
2021-10-21 20:35:04 +0300

    Update sale.py

--- 28exec16/sale.py
+++ 28exec16/sale.py
@@ -1,12 +1,9 @@
 def MaximumDiscount(n, prices):
     discount = 0
-    while len(prices) > 3:
+    while len(prices) >= 3:
         for i in range(3):
             price = max(prices)
             prices.remove(price)
             if i == 2:
                 discount += price
     return discount
-
-
-
