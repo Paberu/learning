d984e4d10ec6c8a25079f497f6ef7566af313727
Paberu <paberu.tocador@gmail.com>
2021-10-25 16:24:48 +0300

    Update shop.py

--- 28exec19/shop.py
+++ 28exec19/shop.py
@@ -11,18 +11,3 @@ def ShopOLAP(n, items):
     sorted_items.reverse()
 
     return sorted_items
-    
-    bought_comp = dict()
-    for value in new_items.values():
-        if value not in bought_comp:
-            bought_comp[value] = True
-        else:
-            bought_comp[value] = False
-    
-    results = ['']*len(new_items)
-    for key, value in new_items.items():
-        if bought_comp[value]:
-            results[items.index(value)] = key + ' ' + str(value)
-    
-
-    return results
