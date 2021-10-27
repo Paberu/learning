f6748974b5c27f11da25228302d0998bdf75ae24
Paberu <paberu.tocador@gmail.com>
2021-10-27 19:17:50 +0300

    Update lapot.py

--- 28exec20/lapot.py
+++ 28exec20/lapot.py
@@ -3,10 +3,9 @@ history = []
 position = 0
 last_operation = ''
 
+
 def BastShoe(command):
     global file, history, position, last_operation
-    #print('----->',file)
-    #print('----->',history)
     s = file
 
     if command[0] in ('1', '2', '3'):
@@ -17,7 +16,7 @@ def BastShoe(command):
     if operation in ('1', '2'):
         if operation == '1':
             s += parameter
-        else: # operation == '2'
+        else:  # operation == '2'
             index = int(parameter)
             if index:
                 s = s[:-index]
@@ -52,36 +51,3 @@ def BastShoe(command):
         return s
     else:
         return s
-
-
-print(1,BastShoe('1 Привет'))
-print(2,BastShoe('1  , Мир!'))
-print(3,BastShoe('1 ++'))
-print(4,BastShoe('2 2'))
-print(5,BastShoe('4'))
-print(6,BastShoe('4'))
-print(7,BastShoe('1 *'))
-print(8,BastShoe('4'))
-print(9,BastShoe('4'))
-print(10,BastShoe('4'))
-print(11,BastShoe('3 6'))
-print(12,BastShoe('2 100'))
-
-print(13,BastShoe('1 Привет'))
-print(14,BastShoe('1  , Мир!'))
-print(15,BastShoe('1 ++'))
-print(16,BastShoe('4'))
-print(17,BastShoe('4'))
-print(18,BastShoe('5'))
-print(19,BastShoe('4'))
-print(20,BastShoe('5'))
-print(21,BastShoe('5'))
-print(22,BastShoe('5'))
-print(23,BastShoe('5'))
-print(24,BastShoe('4'))
-print(25,BastShoe('4'))
-print(26,BastShoe('2 2'))
-print(27,BastShoe('4'))
-print(28,BastShoe('5'))
-print(29,BastShoe('5'))
-print(30,BastShoe('5'))
