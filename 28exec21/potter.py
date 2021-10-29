ba8f505a8acdfd263e17068a403a1211689a0c38
Paberu <paberu.tocador@gmail.com>
2021-10-29 16:03:31 +0300

    Create potter.py

---
+++ 28exec21/potter.py
@@ -0,0 +1,30 @@
+def BiggerGreater(word):
+    output = ''
+    if word == max(word)*len(word):
+        return ''
+
+    for i in range(len(word)-1, 1, -1):
+        if word[i] > word[i-1]:
+            output = word[:i-1] + word[i] + word[i-1]
+            return output
+    
+    the_letter = max(word)
+    for curr_letter in word:
+        if curr_letter > word[0] and curr_letter < the_letter:
+            the_letter = curr_letter
+
+    temp_word = sorted(word)
+    temp_word.remove(the_letter)
+    output = the_letter + ''.join(temp_word)
+
+    if output <= word:
+        return ''
+    else:
+        return output
+
+print(BiggerGreater("ая"))
+print(BiggerGreater("fff"))
+print(BiggerGreater("нклм"))
+print(BiggerGreater("вибк"))
+print(BiggerGreater("вкиб"))
+print(BiggerGreater("zyxw"))
