4791ed83bd2647af62486351f488bef4cb8f4e4d
Paberu <paberu.tocador@gmail.com>
2021-11-02 23:37:14 +0300

    Update matrix.py

--- 28exec24/matrix.py
+++ 28exec24/matrix.py
@@ -1,33 +1,26 @@
 def MatrixTurn(matrix, m, n, t):
     for i in range(len(matrix)):
-        #matrix[i] = list(map(int, matrix[i]))
         matrix[i] = list(matrix[i])
 
-    for deep in range():
+    for _ in range(t):
+        for deep in range(min(m, n)//2):
+            temp = matrix[deep][n-1-deep]
+            temp2 = matrix[m-1-deep][n-1-deep]
+            temp3 = matrix[m-1-deep][deep]
 
-    temp = matrix[0][n-1]
-    for i in range(n-1, 0 ,-1):
-        matrix[0][i] = matrix[0][i-1]
+            for i in reversed(range(deep, n-deep)):
+                if i > deep:
+                    matrix[deep][i] = matrix[deep][i-1]
 
-    temp2 = matrix[m-1][n-1]
-    for j in range(m-1, 0, -1):
-        matrix[j][n-1] = matrix[j-1][n- 1]
-    matrix[1][n-1] = temp
+            for j in reversed(range(deep, m-deep)):
+                if j > deep:
+                    matrix[j][n-1-deep] = matrix[j-1][n-1-deep]
+            matrix[deep+1][n-1-deep] = temp
 
-    temp3 = matrix[m-1][0]
-    for i in range(n-1):
-        matrix[m-1][i] = matrix[m-1][i+1]
-    matrix[m-1][n-2] = temp2
+            for i in range(deep, n-1-deep):
+                matrix[m-1-deep][i] = matrix[m-1-deep][i+1]
+            matrix[m-1-deep][n-2-deep] = temp2
 
-    temp4 = matrix[0][1]
-    for j in range(m-1):
-        matrix[j][0] = matrix[j+1][0]
-    matrix[m-2][0] = temp3
-
-
-matr = ["123456", "234567", "345678", "456789"]
-for str in matr:
-    print(list(str))
-MatrixTurn(matr, 4, 6, 3)
-for str in matr:
-    print(str)
+            for j in range(deep, m-1-deep):
+                matrix[j][deep] = matrix[j+1][deep]
+            matrix[m-2-deep][deep] = temp3
