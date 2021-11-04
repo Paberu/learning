3f6404ba28919a4c363dfc99244acaf45d882a0d
Paberu <paberu.tocador@gmail.com>
2021-11-04 15:43:44 +0300

    Create transform.py

---
+++ 28exec25/transform.py
@@ -0,0 +1,15 @@
+def TransformTransform(a, n):
+
+    def Transform(a,n):
+        b = []
+        for i in range(n):
+            for j in range(n-i):
+                k = i + j
+                b.append(max(a[j:k+1]))
+        return b
+
+    b = Transform(a,n)
+    if not sum(Transform(b, len(b))) % 2:
+        return True
+    else:
+        return False
