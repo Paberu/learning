5d6879b7e353fe7ed566c8a9cf17b7b6dd5488c5
Paberu <paberu.tocador@gmail.com>
2021-11-07 15:00:36 +0300

    Update hodor.py

--- 28exec26/hodor.py
+++ 28exec26/hodor.py
@@ -1,40 +1,26 @@
 def white_walkers(village):
     start_num = -1
     white_walkers_count = 0
+    found = False
     for folk in village:
         if folk == '=':
-            if start_num == -1:
-                return False
-            else:
-                if white_walkers_count >= 3:
-                    return False
-                else:
-                    white_walkers_count += 1
+            white_walkers_count += 1
         elif folk.isdecimal():
-            if white_walkers_count == 0:
+            if start_num == -1:
                 start_num = int(folk)
+                white_walkers_count = 0
             else:
                 finish_num = int(folk)
                 if finish_num + start_num == 10:
                     if white_walkers_count not in (0, 3):
                         return False
-                    else:
+                    elif white_walkers_count == 3:
                         white_walkers_count = 0
                         start_num = finish_num
-                else:
-                    if white_walkers_count != 0:
-                        return False
+                        found = True
                     else:
                         start_num = finish_num
-
-    if white_walkers_count != 0:
-        return False
-    else:
-        return True
-
-
-print(white_walkers("axxb6===4xaf5===eee5"))
-print(white_walkers("5==ooooooo=5=5"))
-print(white_walkers("abc=7==hdjs=3gg1=======5"))
-print(white_walkers("aaS=8"))
-print(white_walkers("9===1===9===1===9"))
+                else:
+                    white_walkers_count = 0
+                    start_num = finish_num
+    return found
