80622d8dd812fc10cc067e1d08ed684be5397861
Paberu <paberu.tocador@gmail.com>
2022-03-14 23:41:45 +0300

    create_deep_and+wide

---
+++ algo2_3/BST.py
@@ -0,0 +1,153 @@
+class BSTNode:
+
+    def __init__(self, key, val, parent):
+        self.NodeKey = key  # ключ узла
+        self.NodeValue = val  # значение в узле
+        self.Parent = parent  # родитель или None для корня
+        self.LeftChild = None  # левый потомок
+        self.RightChild = None  # правый потомок
+
+    def count_children(self):
+        count = 1  # сам себе уже есть
+        if not self.LeftChild and not self.RightChild:
+            return count
+        else:
+            if self.LeftChild:
+                count += self.LeftChild.count_children()
+            if self.RightChild:
+                count += self.RightChild.count_children()
+        return count
+
+
+class BSTFind:  # промежуточный результат поиска
+
+    def __init__(self):
+        self.Node = None  # None если
+        # в дереве вообще нету узлов
+        self.NodeHasKey = False  # True если узел найден
+        self.ToLeft = False  # True, если родительскому узлу надо
+        # добавить новый узел левым потомком
+
+
+class BST:
+
+    def __init__(self, node):
+        self.Root = node  # корень дерева, или None
+
+    def FindNodeByKey(self, key):
+        node = self.Root
+        find_result = BSTFind()
+        while node:
+            if key == node.NodeKey:
+                find_result.Node = node
+                find_result.NodeHasKey = True
+                return find_result
+            elif key < node.NodeKey:
+                if not node.LeftChild:
+                    find_result.Node = node
+                    find_result.NodeHasKey = False
+                    find_result.ToLeft = True
+                    return find_result
+                else:
+                    node = node.LeftChild
+            else:  # key > node.NodeKey
+                if not node.RightChild:
+                    find_result.Node = node
+                    find_result.NodeHasKey = False
+                    find_result.ToLeft = False
+                    return find_result
+                else:
+                    node = node.RightChild
+        return None
+
+    def AddKeyValue(self, key, val):
+        find_result = self.FindNodeByKey(key)
+        if not find_result:
+            self.Root = BSTNode(key, val, None)
+            return True
+        if find_result.NodeHasKey:
+            return False
+        else:
+            if find_result.ToLeft:
+                find_result.Node.LeftChild = BSTNode(key, val, find_result.Node)
+                return True
+            else:
+                find_result.Node.RightChild = BSTNode(key, val, find_result.Node)
+                return True
+
+    def FinMinMax(self, FromNode, FindMax):
+        node = FromNode
+        if not FindMax:
+            while node.LeftChild:
+                node = node.LeftChild
+        else:
+            while node.RightChild:
+                node = node.RightChild
+        return node
+
+    def DeleteNodeByKey(self, key):
+        find_result = self.FindNodeByKey(key)
+        # если нет узла с указанным ключом, выходим
+        if not find_result or not find_result.NodeHasKey:
+            return False
+        else:
+            node_to_delete = find_result.Node
+            if node_to_delete.RightChild:
+                node_for_replace = node_to_delete.RightChild
+                while node_for_replace.LeftChild:
+                    node_for_replace = node_for_replace.LeftChild
+            else:
+                node_for_replace = node_to_delete.LeftChild
+
+            if not node_for_replace:
+                self.replace_node(node_to_delete, node_for_replace)  # удаление листа
+                return True
+            ex_parent = node_for_replace.Parent
+
+            self.replace_node(node_to_delete, node_for_replace)
+
+            if node_to_delete is not ex_parent:
+                if not node_for_replace.RightChild:
+                    ex_parent.LeftChild = None
+                else:
+                    ex_parent.LeftChild = node_for_replace.RightChild
+                    ex_parent.LeftChild.Parent = ex_parent
+                node_for_replace.RightChild = node_to_delete.RightChild
+                node_for_replace.RightChild.Parent = node_for_replace
+            node_for_replace.LeftChild = node_to_delete.LeftChild
+            node_for_replace.LeftChild.Parent = node_for_replace
+        return True
+
+    def replace_node(self, node_to_delete, node_for_replace):
+        parent_node = node_to_delete.Parent
+        if parent_node:
+            if parent_node.LeftChild == node_to_delete:
+                parent_node.LeftChild = node_for_replace
+            else:  # parent_node.RightChild == node_to_delete
+                parent_node.RightChild = node_for_replace
+            node_for_replace.Parent = node_to_delete.Parent
+        else:
+            self.Root = node_for_replace
+            if node_for_replace:
+                node_for_replace.Parent = None
+
+    def Count(self):
+        if not self.Root:
+            return 0  # количество узлов в дереве
+        else:
+            return self.Root.count_children()
+
+    def WideAllNodes(self):
+        nodes_for_look_through = [self.Root]
+        nodes_for_return = [self.Root]
+        while nodes_for_look_through:
+            tmp_nodes = []
+            for node in nodes_for_look_through:
+                if node.LeftChild:
+                    nodes_for_return.append(node.LeftChild)
+                    tmp_nodes.append(node.LeftChild)
+                if node.RightChild:
+                    nodes_for_return.append()
+                    tmp_nodes.append(node.RightChild)
+            nodes_for_look_through = tmp_nodes
+        return tuple(nodes_for_return)
---
+++ algo2_3/TestBST.py
@@ -0,0 +1,99 @@
+import unittest
+from BST import BSTNode, BSTFind, BST
+
+
+class TestSimpleTree(unittest.TestCase):
+    def setUp(self):
+        self.root1 = BSTNode(11, 11, None)
+        self.bst1 = BST(self.root1)
+
+        self.root2 = BSTNode(11, 11, None)
+        self.bst2 = BST(self.root2)
+        self.bst2.AddKeyValue(9, 9)
+        self.bst2.AddKeyValue(15, 15)
+        self.bst2.AddKeyValue(13, 13)
+        self.bst2.AddKeyValue(17, 17)
+
+        self.root3 = BSTNode(11, 11, None)
+        self.bst3 = BST(self.root3)
+        self.bst3.AddKeyValue(9, 9)
+        self.bst3.AddKeyValue(15, 15)
+        self.bst3.AddKeyValue(13, 13)
+        self.bst3.AddKeyValue(17, 17)
+        self.bst3.AddKeyValue(7, 7)
+        self.bst3.AddKeyValue(10, 10)
+        self.bst3.AddKeyValue(5, 5)
+        self.bst3.AddKeyValue(9.5, 9.5)
+        self.bst3.AddKeyValue(10.5, 10.5)
+        self.bst3.AddKeyValue(14, 14)
+        self.bst3.AddKeyValue(19, 19)
+
+    def test_add_key_value(self):
+        self.assertEqual(self.root1.LeftChild, None)
+        self.assertTrue(self.bst1.AddKeyValue(9, 9))
+        self.assertEqual(self.root1.LeftChild.NodeValue, 9)
+        self.assertEqual(self.root1.RightChild, None)
+        self.assertTrue(self.bst1.AddKeyValue(15, 15))
+        self.assertEqual(self.root1.RightChild.NodeValue, 15)
+        self.assertTrue(self.bst1.AddKeyValue(13, 13))
+        self.assertTrue(self.bst1.AddKeyValue(17, 17))
+        self.assertTrue(self.bst1.AddKeyValue(12, 12))
+        self.assertFalse(self.bst1.AddKeyValue(9, 9))
+
+    def test_find_node_by_key(self):
+        bst_res1 = self.bst2.FindNodeByKey(12)
+        self.assertEqual(bst_res1.Node.NodeValue, 13)
+        self.assertFalse(bst_res1.NodeHasKey)
+        self.assertTrue(bst_res1.ToLeft)
+        bst_res2 = self.bst2.FindNodeByKey(14)
+        self.assertEqual(bst_res2.Node.NodeValue, 13)
+        self.assertFalse(bst_res2.NodeHasKey)
+        self.assertFalse(bst_res2.ToLeft)
+        bst_res3 = self.bst2.FindNodeByKey(13)
+        self.assertEqual(bst_res3.Node.NodeValue, 13)
+        self.assertTrue(bst_res3.NodeHasKey)
+
+    def test_count(self):
+        self.assertEqual(self.bst2.Count(), 5)
+    #
+    # def test_empty_tree(self):
+    #     bst = BST(None)
+    #     self.assertEqual(bst.FindNodeByKey(14), None)
+    #     self.assertFalse(bst.DeleteNodeByKey(11))
+    #     self.assertEqual(bst.Count(), 0)
+    #     self.assertTrue(bst.AddKeyValue(10, 10))
+    #     self.assertEqual(bst.Count(), 1)
+    #     find_result = bst.FindNodeByKey(14)
+    #     self.assertFalse(find_result.NodeHasKey)
+    #     self.assertFalse(find_result.ToLeft)
+    #     self.assertFalse(bst.DeleteNodeByKey(11))
+    #
+    # def test_delete1(self):
+    #     self.assertTrue(self.bst2.DeleteNodeByKey(15))
+    #     self.bst2.AddKeyValue(16, 16)
+    #     self.assertEqual(self.bst2.FindNodeByKey(16).Node.Parent.NodeValue, 13)
+    #
+    # def test_delete_root(self):
+    #     self.assertTrue(self.bst2.DeleteNodeByKey(11))
+    #     self.assertEqual(self.bst2.Root.LeftChild.NodeValue, 9)
+    #     self.assertEqual(self.bst2.Root.RightChild.NodeValue, 15)
+    #
+    # def test_delete2(self):
+    #     self.bst3.DeleteNodeByKey(11)
+    #     self.assertEqual(self.bst3.Root.NodeValue, 13)
+    #
+    # def test_delete_empty_tree(self):
+    #     test_bst = BST(BSTNode(1, 1, None))
+    #     self.assertTrue(test_bst.DeleteNodeByKey(1))
+    #     self.assertEqual(test_bst.Root, None)
+
+    def test_root_leaf_tree(self):
+        test_bst = BST(BSTNode(5, 5, None))
+        test_bst.AddKeyValue(2, 2)
+        self.assertTrue(test_bst.DeleteNodeByKey(5))
+        self.assertEqual(test_bst.Root.NodeValue, 2)
+        self.assertEqual(test_bst.Root.Parent, None)
+        self.assertEqual(test_bst.Root.LeftChild, None)
+        self.assertEqual(test_bst.Root.RightChild, None)
+
+unittest.main()
