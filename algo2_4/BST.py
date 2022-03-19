bca0d567cde42866ffd07bd661176d12f98e1dc7
Paberu <paberu.tocador@gmail.com>
2022-03-19 20:15:09 +0300

    done

--- algo2_4/BST.py
+++ algo2_4/BST.py
@@ -5,10 +5,29 @@ class aBST:
         self.Tree = [None] * tree_size  # массив ключей
 
     def FindKeyIndex(self, key):
-        # ищем в массиве индекс ключа
+        index = 0
+        tree_size = len(self.Tree)
+        while self.Tree[index] is not None and index < tree_size:
+            if self.Tree[index] > key:
+                index = 2*index + 1
+            elif self.Tree[index] < key:
+                index = 2*index + 2
+            else:  # == key
+                return index
+        if self.Tree[index] is None:
+            return -index
         return None  # не найден
 
     def AddKey(self, key):
-        # добавляем ключ в массив
-        return -1;
-        # индекс добавленного/существующего ключа или -1 если не удалось
+        index = self.FindKeyIndex(key)
+        if index < 0:
+            self.Tree[-index] = key
+            return -index
+        elif index > 0:
+            return index
+        else:
+            if not self.Tree[0]:
+                self.Tree[0] = key
+                return 0
+            else:
+                return -1
--- algo2_4/TestBST.py
+++ algo2_4/TestBST.py
@@ -1,68 +1,23 @@
 import unittest
-from BST import BSTNode, BSTFind, BST
+from BST import aBST
 
 
 class TestSimpleTree(unittest.TestCase):
     def setUp(self):
-        self.root1 = BSTNode(11, 11, None)
-        self.bst1 = BST(self.root1)
+        self.bst1 = aBST(3)
+        self.bst2 = aBST(2)
+        self.bst2.Tree = [5, 3, 7]
 
-        self.root2 = BSTNode(11, 11, None)
-        self.bst2 = BST(self.root2)
-        self.bst2.AddKeyValue(9, 9)
-        self.bst2.AddKeyValue(15, 15)
-        self.bst2.AddKeyValue(13, 13)
-        self.bst2.AddKeyValue(17, 17)
+    def test_find_key(self):
+        self.assertEqual(self.bst2.FindKeyIndex(5), 0)
+        self.assertEqual(self.bst2.FindKeyIndex(3), 1)
+        self.assertEqual(self.bst2.FindKeyIndex(7), 2)
 
-        self.root3 = BSTNode(11, 11, None)
-        self.bst3 = BST(self.root3)
-        self.bst3.AddKeyValue(9, 9)
-        self.bst3.AddKeyValue(15, 15)
-        self.bst3.AddKeyValue(13, 13)
-        self.bst3.AddKeyValue(17, 17)
-        self.bst3.AddKeyValue(7, 7)
-        self.bst3.AddKeyValue(10, 10)
-        self.bst3.AddKeyValue(5, 5)
-        self.bst3.AddKeyValue(9.5, 9.5)
-        self.bst3.AddKeyValue(10.5, 10.5)
-        self.bst3.AddKeyValue(14, 14)
-        self.bst3.AddKeyValue(19, 19)
 
-    def test_add_key_value(self):
-        self.assertEqual(self.root1.LeftChild, None)
-        self.assertTrue(self.bst1.AddKeyValue(9, 9))
-        self.assertEqual(self.root1.LeftChild.NodeValue, 9)
-        self.assertEqual(self.root1.RightChild, None)
-        self.assertTrue(self.bst1.AddKeyValue(15, 15))
-        self.assertEqual(self.root1.RightChild.NodeValue, 15)
-        self.assertTrue(self.bst1.AddKeyValue(13, 13))
-        self.assertTrue(self.bst1.AddKeyValue(17, 17))
-        self.assertTrue(self.bst1.AddKeyValue(12, 12))
-        self.assertFalse(self.bst1.AddKeyValue(9, 9))
+    def test_add_key(self):
+        self.assertEqual(self.bst1.AddKey(5), 0)
+        self.assertEqual(self.bst1.AddKey(3), 1)
+        self.assertEqual(self.bst1.AddKey(7), 2)
 
-    def test_find_node_by_key(self):
-        bst_res1 = self.bst2.FindNodeByKey(12)
-        self.assertEqual(bst_res1.Node.NodeValue, 13)
-        self.assertFalse(bst_res1.NodeHasKey)
-        self.assertTrue(bst_res1.ToLeft)
-        bst_res2 = self.bst2.FindNodeByKey(14)
-        self.assertEqual(bst_res2.Node.NodeValue, 13)
-        self.assertFalse(bst_res2.NodeHasKey)
-        self.assertFalse(bst_res2.ToLeft)
-        bst_res3 = self.bst2.FindNodeByKey(13)
-        self.assertEqual(bst_res3.Node.NodeValue, 13)
-        self.assertTrue(bst_res3.NodeHasKey)
-
-    def test_count(self):
-        self.assertEqual(self.bst2.Count(), 5)
-
-    def test_wide_all_nodes(self):
-        # for each in self.bst3.WideAllNodes():
-        #     print(each.NodeValue)
-        pass
-
-    def test_deep_all_nodes(self):
-        for each in self.bst3.DeepAllNodes(3):
-            print(each.NodeValue)
 
 unittest.main()
