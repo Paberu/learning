8d3ba1e1f1e4b586f5ac7f3ca01d82d09ad415cd
Paberu <paberu.tocador@gmail.com>
2022-03-20 22:36:41 +0300

    done

--- algo2_5/BSR_func.py
+++ algo2_5/BSR_func.py
@@ -1,9 +1,15 @@
 def GenerateBBSTArray(unsorted_array):
+    if len(unsorted_array) == 1:
+        return unsorted_array
     sorted_array = sorted(unsorted_array)
-    bbst_array = [None] * len(sorted_array)
-    root_index = int((len(sorted_array) - 1) / 2)  # середина массива
-    bbst_array[0] = sorted_array[root_index]
-    for i in range(1, root_index+1):
-        bbst_array[2*i-1] = sorted_array[root_index-i]
-        bbst_array[2*i] = sorted_array[root_index+i]
+    size = len(sorted_array)
+    bbst_array = []
+    index = step = int((size - 1) / 2)  # середина массива
+    while len(bbst_array) < size:
+        base = index
+        while index < size:
+            bbst_array.append(sorted_array[index])
+            index += step + 1  # +1 из-за того, что в python нумерация индексов с 0
+        index = int((base-1)/2)
+        step = base
     return bbst_array
--- algo2_5/Test_BSR_func.py
+++ algo2_5/Test_BSR_func.py
@@ -3,9 +3,18 @@ from BSR_func import GenerateBBSTArray
 
 
 class TestSimpleTree(unittest.TestCase):
+
     def test_func(self):
-        array = [x for x in range(1, 6)]
-        self.assertEqual(GenerateBBSTArray(array), [3,2,4,1,5])
+        array = [x for x in range(1, 8)]
+        self.assertEqual(GenerateBBSTArray(array), [4, 2, 6, 1, 3, 5, 7])
+
+    def test_func2(self):
+        array = [x for x in range(1, 4)]
+        self.assertEqual(GenerateBBSTArray(array), [2, 1, 3])
+
+    def test_func3(self):
+        array = [1]
+        self.assertEqual(GenerateBBSTArray(array), [1])
 
 
 unittest.main()
