4a54da15b25800121a3e1c0c05682062245cd1aa
Paberu <paberu.tocador@gmail.com>
2023-09-05 23:19:04 +0300

    Create 6.py

---
+++ fp/6.py
@@ -0,0 +1,29 @@
+from pymonad.tools import curry
+from pymonad.state import State
+
+
+user_init = {'items': [], 'money': 2000}
+
+items = {'apples': 70,
+         'wine': 300,
+         'milk': 80,
+         'chips': 100
+         }
+
+user_state = State(user_init['items'], user_init['money'])
+
+
+@curry(2)
+def buy(user_items, item):
+    @State
+    def count_computation(old_count):
+        if old_count < items[item] or item not in items:
+            return user_items, old_count
+        return user_items.append(item), old_count - items[item]
+    return count_computation
+
+
+finale = user_state.then(buy('wine')).then(buy('apples')).then(buy('chips')).then(buy('chips')).then(buy('milk'))
+print(finale.value)
+print(finale.monoid)
+
