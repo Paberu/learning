a69cfd5b9225a16bd8f35326c66b149136ca538b
Paberu <paberu.tocador@gmail.com>
2023-09-25 20:34:04 +0300

    Explicitly given types

--- fp/3.fs
+++ fp/3.fs
@@ -1,4 +1,4 @@
-let days_in_month = function 
+let days_in_month : int -> int = function
     |1|3|5|7|8|10|12 -> 31
     |4|6|9|11 -> 30
     |2 -> 28
--- fp/3_check.fs
+++ fp/3_check.fs
@@ -1,7 +1,7 @@
 //F# Compiler for F# 4.0 (Open Source Edition)
 
 open System
-let days_in_month = function 
+let days_in_month : int -> int = function
     |1|3|5|7|8|10|12 -> 31
     |4|6|9|11 -> 30
     |2 -> 28
