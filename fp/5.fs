3df249997168ad071e53db088be0534a9646c846
Paberu <paberu.tocador@gmail.com>
2023-09-28 23:44:49 +0300

    Create 5.fs

---
+++ fp/5.fs
@@ -0,0 +1,11 @@
+// 16.1
+let notDivisible : int * int -> bool = function
+    |(m,n) when n = 0 -> false
+    |(m,n) -> (m % n = 0)
+
+// 16.2
+let prime (n: int) : bool =
+    let rec check : int -> bool = function
+        |i when i = 2 -> n % 2 <> 0 && check (i + 1)
+        |i -> i > n/2 || (n % i <> 0 && check (i + 2))
+    check 2
