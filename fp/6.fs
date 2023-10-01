fcb5ad9a3e551938562a18e08a196ced11222472
Paberu <paberu.tocador@gmail.com>
2023-10-01 17:42:39 +0300

    Create 6.fs

---
+++ fp/6.fs
@@ -0,0 +1,17 @@
+// 17.1
+let rec pow : string * int -> string = function
+    |(s,1) -> s
+    |(s,n) -> s+pow(s,n-1)
+
+// 17.2
+let rec isIthChar : string * int * char -> bool = function
+    |(s,n,c) -> s.[n] = c
+
+// 17.3
+let rec countChar : string * int * char * int -> int = function
+    |(str,position,ch,total) when position = String.length(str) -> total
+    |(str,position,ch,total) when isIthChar(str,position,ch) -> countChar(str,position+1,ch,total+1)
+    |(str,position,ch,total) -> countChar(str,position+1,ch,total)
+
+let rec occFromIth : string * int * char -> int = function
+    |(s,n,c) -> countChar(s,n,c,0)
