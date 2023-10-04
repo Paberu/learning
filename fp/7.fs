e45f26325e838abf39e3701051ae966a904684aa
Paberu <paberu.tocador@gmail.com>
2023-10-04 22:17:54 +0300

    Create 7.fs

---
+++ fp/7.fs
@@ -0,0 +1,10 @@
+// 20.3.1
+let vat n x = x * (1.0 + (float n)/100.0)
+
+// 20.3.2
+let unvat n x = x * (1.0 - (float n)/100.0)
+
+// 20.3.3
+let rec min f = 
+    let rec check x f = if f x = 0 then x else check (x+1) f
+    check 1 f
