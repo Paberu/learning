367ef8c2a9985d9bdc8c61431730873567e26d42
Paberu <paberu.tocador@gmail.com>
2023-10-17 21:53:00 +0300

    Update 13.fs

--- fp/13.fs
+++ fp/13.fs
@@ -1,10 +1,12 @@
 // 39.1
-let rec rmodd = function
+let rec rmodd inputList =
+    match inputList with
     | [] | [_] -> []
     | _ :: x :: tail -> x :: rmodd tail
 
 // 39.2
-let rec del_even = function
+let rec del_even inputList =
+    match inputList with
     | [] -> []
     | x :: tail when x % 2 = 1 -> x :: del_even tail
     | _ :: tail -> del_even tail
@@ -35,4 +37,3 @@ let rec zip (xs1,xs2) =
         match (xs1, xs2) with
             | ([],[]) -> []
             | (x1 :: tail1, x2 :: tail2) -> x1 :: x2 :: zip (tail1, tail2)
-    
