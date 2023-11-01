ebc0b10aa4c1bea503d0478e06ebbb2b08bf471c
Paberu <paberu.tocador@gmail.com>
2023-11-01 07:58:51 +0300

    Update 17.fs

--- fp/17.fs
+++ fp/17.fs
@@ -1,8 +1,4 @@
+// 43.3
 let try_find key m = 
-    let rec look_through inner_key key_list =
-        match key_list with
-        | [] -> None
-        | x::tail when x = inner_key -> Some(Map.find x m)
-        | _::tail -> look_through inner_key tail
-    look_through key (Map.keys m)
-
+    if Map.containsKey key m then Some(Map.find key m)
+    else None
