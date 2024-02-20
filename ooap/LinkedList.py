f41f248cf27e69392cd9367bd97ec1e4111d1a8e
Paberu <paberu.tocador@gmail.com>
2024-02-20 17:39:58 +0300

    Написал код, написал ответы, но не всё покрыл тестами.

--- ooap/LinkedList.py
+++ ooap/LinkedList.py
@@ -1,29 +1,57 @@
+from typing import Final
+
+
 class LinkedList:
 
     LIST_EMPTY: Final = 0
     LIST_NOT_EMPTY: Final = 1
-    CURSOR_NIL: Final = 0
-    CURSOR_OK: Final = 1
-    CURSOR_ERR: Final = 2
+    FIND_NIL: Final = 0
+    FIND_OK: Final= 1
+    FIND_ERR: Final = 2
 
     def __init__(self):
         self.__list = []
-        self.__cursor = None  # указывает на индекс текущего элемента в списке
+        self.__cursor = 0  # указывает на индекс текущего элемента в списке
 
         self.__list_status = LinkedList.LIST_EMPTY
-        self.__cursor_status = LinkedList.CURSOR_NIL
+        self.__find_status = LinkedList.FIND_NIL
 
-    # запрос
+    # запросы:
     def size(self):
         return len(self.__list)
 
     # предусловие: список должен быть непустой
-    # постусловие: возврат текущего значения
     def get(self):
-        if LinkedList.LIST_NOT_EMPTY:
+        if self.__list_status == LinkedList.LIST_NOT_EMPTY:
             return self.__list[self.__cursor]
 
-    # команды
+    # предусловие: список должен быть непустой
+    def is_head(self):
+        if self.__list_status == LinkedList.LIST_NOT_EMPTY:
+            return self.__cursor == 0
+
+    # предусловие: список должен быть непустой
+    def is_tail(self):
+        if self.__list_status == LinkedList.LIST_NOT_EMPTY:
+            return self.__cursor == self.size() - 1
+
+    # предусловие: список должен быть непустой
+    def is_value(self):
+        if self.__list_status == LinkedList.LIST_NOT_EMPTY:
+            return True
+
+    # предусловие: список должен быть непустой
+    def find(self, value):
+        if self.__list_status == LinkedList.LIST_NOT_EMPTY:
+            try:
+                result = self.__list.index(value, self.__cursor)
+                self.__find_status = LinkedList.FIND_OK
+            except ValueError:
+                result = 0
+                self.__find_status = LinkedList.FIND_ERR
+            return result
+
+    # команды:
 
     # предусловие: список должен быть непустой
     # постусловие: курсор сместился к началу списка
@@ -40,7 +68,7 @@ class LinkedList:
     # предусловие: справа должен быть хотя бы один элемент
     # постусловие: курсор сместится к элементу справа
     def right(self):
-        if self.__list_status == LinkedList.LIST_NOT_EMPTY and self.__cursor < size() - 1:
+        if self.__list_status == LinkedList.LIST_NOT_EMPTY and self.__cursor < self.size() - 1:
             self.__cursor += 1
 
     # почему-то отсутствовал вариант с движением в обратную сторону
@@ -56,13 +84,14 @@ class LinkedList:
     #  - элемент не последний
     # постусловие: добавление элемента в конец списка или создание нового списка и замена им старого
     def put_right(self, value):
-        if self.__list_status == LinkedList.LIST_NOT_EMPTY or self.size() == cursor + 1:
+        if self.__list_status == LinkedList.LIST_EMPTY or self.__cursor == self.size() - 1:
             self.__list.append(value)
         else:
             new_list = self.__list[:self.__cursor+1]
             new_list.append(value)
             new_list.extend(self.__list[self.__cursor+1])
             self.__list = new_list
+        self.__list_status = LinkedList.LIST_NOT_EMPTY  # если добавляли в пустой список
 
     # предусловие: требуется рассмотреть несколько граничных условий:
     #  - пустой список
@@ -79,44 +108,41 @@ class LinkedList:
             new_list.append(value)
             new_list.extend(self.__list[self.__cursor])
             self.__list = new_list
+        self.__list_status = LinkedList.LIST_NOT_EMPTY  # если добавляли в пустой список
 
     # предусловие: требуется рассмотреть несколько граничных условий:
     #  - непустой список и элемент крайний справа (элемент удаляется, курсор уходит на 1 влево)
     #  - просто непустой список (элемент удаляется, курсор остаётся неизменен)
     # постусловие: удаление элемента из списка
     def remove(self):
-        if self.size() != 0:
+        if self.size() in (0, 1):  # пустой список или список из одного элемента, операция очищает список
+            self.clear()
+        else:
             self.__list.pop(self.__cursor)  # удаляет элемент, на который указывает курсор, происходит уменьшение списка на 1
-        if self.__cursor == self.size():  # если элемент был крайний справа, курсор уменьшится на 1
-            self.__cursor -= 1
+
+            if self.__cursor == self.size():  # если элемент был крайний справа, курсор уменьшится на 1
+                self.__cursor -= 1
 
     # постусловие: список пуст, курсор не определён
     def clear(self):
-        self.__init__(self)
+        self.__init__()
 
+    # постусловие: список увеличился на единицу
     def add_tail(self, value):
         self.__list.append(value)
 
+    # предусловие: список должен быть непустой
+    # постусловие: значение узла списка изменилось
     def replace(self, value):
-        self.__list[self.__cursor] = value
+        if self.__list_status == LinkedList.LIST_NOT_EMPTY:
+            self.__list[self.__cursor] = value
 
-    def find(self, value):
-        try:
-            self.__cursor = self.__list.index(value, self.__cursor)
-        except ValueError:
-            pass
-
+    # предусловие: список должен быть непустой
     def remove_all(self, value):
-        self.
-
-    def is_head(self):
-        if self.size() > 0:
-            return self.__cursor == 0
-
-    def is_tail(self):
-        if self.size() > 0:
-            return self.__cursor == self.size() - 1
-
-    def is_value(self):
-        if self.size() > 0:
-            return True
+        if self.__list_status == LinkedList.LIST_NOT_EMPTY:
+            self.__cursor = 0
+            self.__find_status = LinkedList.FIND_NIL
+            self.__cursor = self.find(value)
+            while self.__find_status != LinkedList.FIND_ERR or self.__list_status == LinkedList.LIST_NOT_EMPTY:
+                self.remove()
+                self.__cursor = self.find(value)
