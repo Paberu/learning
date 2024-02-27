0deadb9eebacc7b679174e48187ff484ad3bf4e3
Paberu <paberu.tocador@gmail.com>
2024-02-27 06:58:14 +0300

    Вынес класс Node в отдельный файл. Написал базовый функционал очереди.
    Хочу попробовать 2 варианта: на узлах и на внутреннем массиве.

---
+++ ooap/Queue.py
@@ -0,0 +1,56 @@
+from typing import Final
+from Node import Node
+
+
+class Queue:
+
+    HEAD_NIL: Final = 0
+    HEAD_OK: Final = 1
+    TAIL_NIL: Final = 0
+    TAIL_OK: Final = 1
+    DEQUEUE_NIL: Final = 0
+    DEQUEUE_OK: Final = 1
+    DEQUEUE_ERR: Final = 2
+
+    def __init__(self):
+        self._head = None
+        self._tail = None
+        self._depth = None
+        self._head_status = self.HEAD_NIL
+        self._tail_status = self.TAIL_NIL
+        self._dequeue_status = self.DEQUEUE_NIL
+
+    # команда
+    # постусловие: увеличение очереди на 1 элемент (добавление в хвост очереди)
+    def _enqueue(self, value):
+        new_node = Node(value)
+        if self._head_status == self.HEAD_NIL:
+            self._head = new_node
+            self._head_status = self.HEAD_OK
+        else:
+            self._tail.set_next(new_node)
+            new_node.set_previous(self._tail)
+        self._tail = new_node
+        self._tail_status = self.TAIL_OK
+        self._depth += 1
+
+    # запрос
+    # предусловие: непустая очередь
+    # постусловие: возврат значения из головы очереди (уменьшение очереди на 1 элемент)
+    def _dequeue(self):
+        if self._head_status == self.HEAD_OK:
+            value = self._head.get_value()
+            if self._depth == 1:
+                self.__init__()
+            else:
+                self._head = self._head.get_next()
+                self._head.set_previous(None)
+                self.depth -= 1
+            self._dequeue_status = self.DEQUEUE_OK
+            return value
+        self._dequeue_status = self.DEQUEUE_ERR
+        return 0
+    
+
+
+
