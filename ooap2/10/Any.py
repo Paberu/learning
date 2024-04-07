bd0a72d8302cd04376608c059152fcf60ea84ce3
Paberu <paberu.tocador@gmail.com>
2024-04-07 20:16:05 +0300

    Кажется, простое задание.

---
+++ ooap2/10/Any.py
@@ -0,0 +1,16 @@
+from General import General
+
+
+class Any(General):
+
+    # можно переопределять, системе всё равно
+    def print(self):
+        print('str from Any')
+        super().print()
+
+    # всё, что даёт декоратор @final - это предупреждение от IDE:
+    # 'General.serialize' is marked as '@final' and should not be overridden
+    # гарантированной защиты от несанкционированного переопределения в Python нет
+    def serialize(self):
+        print('Overriding General method.')
+        super().serialize()
