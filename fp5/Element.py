30536d1cff35af9406ad5702a040448c442f3399
Paberu <paberu.tocador@gmail.com>
2025-10-19 15:22:27 +0300

    Create Element.py

---
+++ fp5/Element.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class Element:
+    symbol: str
+    EMPTY = '0'
+
+    def __init__(self, c: str):
+        object.__setattr__(self, 'symbol', c)
+
+    @classmethod
+    def empty(cls):
+        return Element(cls.EMPTY)
