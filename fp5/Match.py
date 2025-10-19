fc7dfb8076be89e2c30c85c762241f68ab07b7a1
Paberu <paberu.tocador@gmail.com>
2025-10-19 18:46:11 +0300

    Update Match.py

--- fp5/Match.py
+++ fp5/Match.py
@@ -17,52 +17,52 @@ class Match:
     col: int
     length: int
 
-    def add_match_if_valid(matches: List[Match], row: int, col: int, length: int, direction: MatchDirection):
-        # Учитываем только комбинации из 3 и более элементов
-        if length >= 3:
-            matches.append(Match(direction, row, col, length))
+def add_match_if_valid(matches: List[Match], row: int, col: int, length: int, direction: MatchDirection):
+    # Учитываем только комбинации из 3 и более элементов
+    if length >= 3:
+        matches.append(Match(direction, row, col, length))
 
-    def find_matches(board: 'Board') -> List[Match]:
-        matches: List[Match] = []
+def find_matches(board: 'Board') -> List[Match]:
+    matches: List[Match] = []
 
-        size = board.size
+    size = board.size
 
-        # Горизонтальные комбинации
-        for row in range(size):
-            start_col = 0
-            for col in range(1, size):
-                if board.cells[row][start_col].symbol == Element.EMPTY:
-                    start_col = col
-                    continue
+    # Горизонтальные комбинации
+    for row in range(size):
+        start_col = 0
+        for col in range(1, size):
+            if board.cells[row][start_col].symbol == Element.EMPTY:
+                start_col = col
+                continue
 
-                if board.cells[row][col].symbol == Element.EMPTY:
-                    add_match_if_valid(matches, row, start_col, col - start_col, MatchDirection.Horizontal)
-                    start_col = col + 1
-                    continue
+            if board.cells[row][col].symbol == Element.EMPTY:
+                add_match_if_valid(matches, row, start_col, col - start_col, MatchDirection.Horizontal)
+                start_col = col + 1
+                continue
 
-                if board.cells[row][col].symbol != board.cells[row][start_col].symbol:
-                    add_match_if_valid(matches, row, start_col, col - start_col, MatchDirection.Horizontal)
-                    start_col = col
-                elif col == size - 1:
-                    add_match_if_valid(matches, row, start_col, col - start_col + 1, MatchDirection.Horizontal)
+            if board.cells[row][col].symbol != board.cells[row][start_col].symbol:
+                add_match_if_valid(matches, row, start_col, col - start_col, MatchDirection.Horizontal)
+                start_col = col
+            elif col == size - 1:
+                add_match_if_valid(matches, row, start_col, col - start_col + 1, MatchDirection.Horizontal)
 
-        # Вертикальные комбинации
-        for col in range(size):
-            start_row = 0
-            for row in range(1, size):
-                if board.cells[start_row][col].symbol == Element.EMPTY:
-                    start_row = row
-                    continue
+    # Вертикальные комбинации
+    for col in range(size):
+        start_row = 0
+        for row in range(1, size):
+            if board.cells[start_row][col].symbol == Element.EMPTY:
+                start_row = row
+                continue
 
-                if board.cells[row][col].symbol == Element.EMPTY:
-                    add_match_if_valid(matches, start_row, col, row - start_row, MatchDirection.Vertical)
-                    start_row = row + 1
-                    continue
+            if board.cells[row][col].symbol == Element.EMPTY:
+                add_match_if_valid(matches, start_row, col, row - start_row, MatchDirection.Vertical)
+                start_row = row + 1
+                continue
 
-                if board.cells[row][col].symbol != board.cells[start_row][col].symbol:
-                    add_match_if_valid(matches, start_row, col, row - start_row, MatchDirection.Vertical)
-                    start_row = row
-                elif row == size - 1:
-                    add_match_if_valid(matches, start_row, col, row - start_row + 1, MatchDirection.Vertical)
+            if board.cells[row][col].symbol != board.cells[start_row][col].symbol:
+                add_match_if_valid(matches, start_row, col, row - start_row, MatchDirection.Vertical)
+                start_row = row
+            elif row == size - 1:
+                add_match_if_valid(matches, start_row, col, row - start_row + 1, MatchDirection.Vertical)
 
-        return matches
+    return matches
