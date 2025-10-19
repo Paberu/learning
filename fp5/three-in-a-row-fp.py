a88fc3348b5b082b5cb9d0ee69f6748b98004843
Paberu <paberu.tocador@gmail.com>
2025-10-19 13:45:31 +0300

    Поменял Board и добавил отрисовку Game

--- fp5/three-in-a-row-fp.py
+++ fp5/three-in-a-row-fp.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from random import random
 
 @dataclass(frozen=True)
 class Element:
@@ -12,4 +13,35 @@ class Element:
     def empty(cls):
         return Element(cls.EMPTY)
 
+class Board:
+    def __init__(self, size: int):
+        self.size = size
+        # Создаём двумерный список размером size x size, заполненный элементами с символом EMPTY
+        self.cells = [[Element(Element.EMPTY) for _ in range(size)] for _ in range(size)]
+
+@dataclass(frozen=True)
+class BoardState:
+    board: Board
+    score: int
+
+class Game:
+    symbols = ['A', 'B', 'C', 'D', 'E', 'F']
+
+    @staticmethod
+    def draw(board):
+        print("  0 1 2 3 4 5 6 7")
+        for i in range(8):
+            print(f"{i} ", end='')
+            for j in range(8):
+                print(board.cells[i][j].symbol, end=' ')
+            print()
+        print()
+
+    @staticmethod
+    def clone_board(board):
+        b = Board(board.size)
+        for row in range(board.size):
+            for col in range(board.size):
+                b.cells[row][col] = board.cells[row][col]
+        return b
 
