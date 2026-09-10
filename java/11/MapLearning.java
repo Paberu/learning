247db5fa57393bbb975e39a92ed28eaf84dceabc
Paberu <paberu.tocador@gmail.com>
2026-09-10 23:26:49 +0300

    Изучение словарей.

--- java/11/MapLearning.java
+++ java/11/MapLearning.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Random;
 
@@ -34,9 +35,28 @@ public class MapLearning {
         System.out.println(this.hashMap.size());
     }
 
+    public int[] useHashMap2(int[] intValues, int frequency) {
+        HashMap<Integer, Integer> countFrequency = new HashMap<>();
+        for (int value : intValues) {
+            countFrequency.merge(value, 1, Integer::sum);
+        }
+        countFrequency.values().removeIf(v -> v < frequency);
+        return countFrequency.keySet().stream().mapToInt(Integer::intValue).toArray();
+    }
+
     static void main() {
         MapLearning ml = new MapLearning();
         ml.learnHashMap1();
+
+        int[] intValues = new int[100];
+        Random random = new Random();
+        for (int i = 0; i < 100; i++) {
+            intValues[i] = random.nextInt(1,11);
+        }
+        int[] goalValues = ml.useHashMap2(intValues, 10);
+        for (int i : goalValues) {
+            System.out.println(i);
+        }
 
     }
 }
