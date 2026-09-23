b47ccce7259eff53ecfdfeb084f031f6acb67472
Paberu <paberu.tocador@gmail.com>
2026-09-23 12:49:42 +0300

    Update LinkedList2.java

--- java/algo/LinkedList2/LinkedList2.java
+++ java/algo/LinkedList2/LinkedList2.java
@@ -171,14 +171,12 @@ public class LinkedList2 {
     }
 }
 
-class Node
-{
+class Node {
     public int value;
     public Node next;
     public Node prev;
 
-    public Node(int _value)
-    {
+    public Node(int _value) {
         value = _value;
         next = null;
         prev = null;
