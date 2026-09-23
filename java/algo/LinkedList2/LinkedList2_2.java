99f6ec60db66303d85444d63e199f1e310c8fdb5
Paberu <paberu.tocador@gmail.com>
2026-09-23 17:15:25 +0300

    Update LinkedList2_2.java

--- java/algo/LinkedList2/LinkedList2_2.java
+++ java/algo/LinkedList2/LinkedList2_2.java
@@ -27,14 +27,25 @@ public class LinkedList2_2 {
         if (linkedList2.head == null || linkedList2.head == linkedList2.tail) {
             return;
         }
-        Node stableNode = linkedList2.tail;
-        Node movingNode = stableNode.prev;
-        Node tempNode = movingNode.prev;
+        Node exHead = linkedList2.head;     // сохраняется указатель на прежний головной узел
+        linkedList2.head = linkedList2.tail;
+
+        Node stableNode = linkedList2.tail; // с чего начинается обход списка - с хвоста
+        Node movingNode = stableNode.prev;  // перемещается относительно хвоста последняя из нетронутых
+        Node tempNode = movingNode.prev;    // временный указатель на предпредпоследнюю, чтобы не потерять
 
         while (tempNode != null) {
-            stableNode.next = stableNode.prev;
+            movingNode.next = null;
+            movingNode.prev = stableNode;
+
+            stableNode = stableNode.prev;
+            movingNode = movingNode.prev;
             tempNode = tempNode.prev;
         }
+
+        linkedList2.tail = movingNode;
+        linkedList2.tail.next = null;       // хвост становится головой
+        linkedList2.head.prev = null;
 
     }
 }
